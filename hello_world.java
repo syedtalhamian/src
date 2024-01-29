0342f37c1170208abfe5e8c7b8930fba21b58569
Syed Talha Mian <129749781+syedtalhamian@users.noreply.github.com>
2024-01-29 13:25:40 +0530

    01 print hello world using java

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,9 @@
 // Your First Program
-
-class hello_world {
+//make sure that the class name matches the name of the file
+class hello_world { 
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        //this will print the text written in the quotes
+        //you can replace the Hello, World! and print any other string as you wish
     }
 }
