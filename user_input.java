ad2ab4d7b1af5cd152070120d82353573487b3f2
Syed Talha Mian <129749781+syedtalhamian@users.noreply.github.com>
2024-01-29 13:48:23 +0530

    02 take user input by keyboard in java

--- user_input.java
+++ user_input.java
@@ -1,12 +1,29 @@
-import java.util.Scanner;
+import java.util.Scanner; //Importing The Java Scanner Class
+
+/*it is important to import Scanner class for getting 
+the user input through keyboard*/
+
 public class user_input {
     public static void main(String[] args) {
-        System.out.println("it works");
+
         Scanner sc = new Scanner(System.in);
+        //this will create the Scanner object, it is neccessary to create one
+       
+        System.out.println("it works");
+        //printing this to ensure that over code has started executing
+
         System.out.println("enter a number");
+        //asking the user to enter an input
+
         int a = sc.nextInt();
+        //taking the user input and storing it in "a"
+        /*we have used the "nextInt" method because we are 
+        getting a Integer as input  */
         System.out.println("your number is ");
+
         System.out.println(a);
+        //this will print the input of the user
+
 
     }
 }
